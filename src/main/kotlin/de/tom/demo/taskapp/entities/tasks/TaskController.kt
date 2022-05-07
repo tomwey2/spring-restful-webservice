37c0a8a5d7504e2b40f05e0d2bdfbb575edab573
Thomas <tomwey2@gmail.com>
2022-05-07 20:03:16 +0400

    documented new rest api function for task

--- src/main/kotlin/de/tom/demo/taskapp/entities/tasks/TaskController.kt
+++ src/main/kotlin/de/tom/demo/taskapp/entities/tasks/TaskController.kt
@@ -21,18 +21,87 @@ class TaskController(val service: TaskService, val userService: UserService, val
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     /**
+     * GET /api/tasks/
+     *
      * Get all tasks of the logged-in user from the database.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @return list of tasks
+     *
+     * @sample
+     * Request:
+     * GET /api/tasks/
+     * Content type: application/json
+     * Authorization: Bearer Token: access token from login
+     *
+     * Response:
+     * Content type: application/json
+     * Code 201 - Ok: See example for response body below
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     *
+     * Example value of response content:
+     * {
+     *      [
+     *          {
+     *              "id": "6274b0f846439e7351056517",
+     *              "text": "Food shopping",
+     *              "description": "One time in week food must be bought.",
+     *              "day": "2022-03-01", "reminder": true,
+     *              "state": "Created", "labels": [],
+     *              "assignees": [],
+     *              "reportedBy": "/api/users/6274b02b46439e7351056510",
+     *              "consistOf": "/api/projects/6274b02a46439e735105650f",
+     *              "createdAt": "2022-05-06T09:24:08.66346", "updatedAt": null
+     *          },
+     *          ...
+     *      ]
+     *  }
      */
     @GetMapping(path = ["/"])
     @ResponseStatus(HttpStatus.OK)
     fun getAll(): List<Task> = service.getAllTasksReportedByUser(userService.getLoggedInUser())
 
     /**
+     * GET /api/tasks/{id}
+     *
      * Get the tasks with the id of the logged-in user from the database.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @param id id of the requested task
+     * @return the task with id if exists otherwise error message
+     *
+     * @sample
+     * Request:
+     * GET /api/tasks/6274b0f846439e7351056517
+     * Authorization: Bearer Token: access token from login
+     * Content type: application/json
+     *
+     * Response:
+     * Content type: application/json
+     * Code 201 - Ok: See example for response body below
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Task with id ... not found
+     *
+     * Example value of response content:
+     *  {
+     *      "id": "6274b0f846439e7351056517",
+     *      "text": "Food shopping",
+     *      "description": "One time in week food must be bought.",
+     *      "day": "2022-03-01", "reminder": true,
+     *      "state": "Created", "labels": [],
+     *      "assignees": [],
+     *      "reportedBy": "/api/users/6274b02b46439e7351056510",
+     *      "consistOf": "/api/projects/6274b02a46439e735105650f",
+     *      "createdAt": "2022-05-06T09:24:08.66346", "updatedAt": null
+     *  }
      */
     @GetMapping(path = ["/{id}"])
     @ResponseStatus(HttpStatus.OK)
@@ -48,9 +117,12 @@ class TaskController(val service: TaskService, val userService: UserService, val
      *
      * @sample
      * Request:
+     * POST /api/tasks/
      * Content type: application/json
+     * Authorization: Bearer Token: access token from login
+     * Body: JSON data: task data
      *
-     * Example value of body:
+     * Example value of request body:
      *  {
      *      "text": "New Task",
      *      "day": "2022-03-01",
@@ -60,8 +132,14 @@ class TaskController(val service: TaskService, val userService: UserService, val
      *
      * Response:
      * Content type: application/json
+     * Code 201 - Ok: See example for response body below
      *
-     * Code 201: Successful operation:
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Project with id ... not found
+     * Code 400 - Bad Request: JSON parse error ...
+     *
      * Example value of response content:
      *  {
      *      "id": "6274b0f846439e7351056517",
@@ -78,9 +156,6 @@ class TaskController(val service: TaskService, val userService: UserService, val
      *      },
      *      "createdAt": "2022-05-06T09:24:08.66346", "updatedAt": null
      *  }
-
-     * Code 404: Project not found
-     * Code 400: Bad Request
      */
     @PostMapping(path = ["/"])
     @ResponseStatus(HttpStatus.CREATED)
@@ -95,18 +170,79 @@ class TaskController(val service: TaskService, val userService: UserService, val
         }
 
     /**
+     * DELETE /api/tasks/{id}
+     *
      * Remove a tasks with id from the database.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @param id id of the requested task
+     * @return code 200 if task is deleted otherwise an error message
+     *
+     * @sample
+     * Request:
+     * DELETE /api/tasks/6274b0f846439e7351056517
+     * Authorization: Bearer Token: access token from login
+     * Content type: application/json
+     *
+     * Response:
+     * Code 201 - Ok
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Task with id ... not found
      */
     @DeleteMapping(path = ["/{id}"])
     @ResponseStatus(HttpStatus.OK)
     fun delete(@PathVariable id: String): Unit = service.deleteTaskOfUser(id, userService.getLoggedInUser())
 
     /**
+     * PUT /api/tasks/6274b0f846439e7351056517
+     *
      * Update the content of a task with the id.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @param body task data
+     * @return updated task
+     *
+     * @sample
+     * Request:
+     * PUT /api/tasks/6274b0f846439e7351056517
+     * Content type: application/json
+     * Authorization: Bearer Token: access token from login
+     * Body: JSON data: task data
+     *
+     * Example value of request body:
+     *  {
+     *      "text": "Updated Task",
+     *      "day": "2022-03-03",
+     *      "reminder": false,
+     *      "projectName": "p1"
+     *  }
+     *
+     * Response:
+     * Content type: application/json
+     * Code 200 - Ok: See example for response body below
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Task with id ... not found
+     *
+     * Example value of response content:
+     *  {
+     *      "id": "6274b0f846439e7351056517",
+     *      "text": "Updated Task",
+     *      "description": "One time in week food must be bought.",
+     *      "day": "2022-03-03", "reminder": false,
+     *      "state": "Created", "labels": [],
+     *      "assignees": [],
+     *      "reportedBy": "/api/users/6274b02b46439e7351056510",
+     *      "consistOf": "/api/projects/6274b02a46439e735105650f",
+     *      "createdAt": "2022-05-06T09:24:08.66346", "updatedAt": null
+     *  }
      */
     @PutMapping(path = ["/{id}"])
     @ResponseStatus(HttpStatus.OK)
@@ -115,9 +251,29 @@ class TaskController(val service: TaskService, val userService: UserService, val
             userService.getLoggedInUser())
 
     /**
+     * GET /api/tasks/{id}/reported
+     *
      * Get the user who has reported the task with id.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @param id id of the requested task
+     * @return the user that has reported the task with id.  if not exists then returns error message.
+     *
+     * @sample
+     * Request:
+     * GET /api/tasks/6274b0f846439e7351056517/reportedby
+     * Authorization: Bearer Token: access token from login
+     * Content type: application/json
+     *
+     * Response:
+     * Content type: application/json
+     * Code 201 - Ok: See GET /api/tasks/{id}
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Task with id ... not found
      */
     @GetMapping(path = ["/{id}/reportedby"])
     @ResponseStatus(HttpStatus.OK)
@@ -126,9 +282,41 @@ class TaskController(val service: TaskService, val userService: UserService, val
             .reportedBy
 
     /**
+     * GET /api/tasks/{id}/assignees
+     *
      * Get the list of users that are assigned to the task with id.
      * The task must be reported by the logged-in user otherwise the server
      * returns an error response.
+     *
+     * @param id id of the requested task
+     * @return the list of user that are assigned to the task with id.
+     *          if not exists then returns error message.
+     *
+     * @sample
+     * Request:
+     * GET /api/tasks/6274b0f846439e7351056517/assignees
+     * Authorization: Bearer Token: access token from login
+     * Content type: application/json
+     *
+     * Response:
+     * Content type: application/json
+     * Code 201 - Ok:
+     *
+     * Error cases:
+     * Code 401 - Unauthorized: Full authentication is required to access this resource
+     * Code 401 - Unauthorized: The Token has expired on ...
+     * Code 404 - Not found: Task with id ... not found
+     *
+     * {
+     *      [
+     *          {
+     *              "id": "6274b02b46439e7351056510", "name": "John Doe", "password": "...",
+     *              "email": "john.doe@test.com", "roles": ["ROLE_USER"],
+     *              "createdAt": "2022-05-06T09:20:43.174","updatedAt": null
+     *          }
+     *      ]
+     *  },
+
      */
     @GetMapping(path = ["/{id}/assignees"])
     @ResponseStatus(HttpStatus.OK)
